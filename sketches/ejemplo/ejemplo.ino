1901d4300b51fcdbef69e0f52985d5941eb70b4e
trondrex <trondrex@gmail.com>
2016-09-18 09:47:25 -0500

    2da clase
    Se comunicaron dos arduinos y se creo un chat entre estos

---
+++ sketches/ejemplo/ejemplo.ino
@@ -0,0 +1,19 @@
+char incomingByte=0;
+void setup() {
+Serial.begin(9600);
+Serial1.begin(9600);
+}
+
+void loop() {
+
+  if(Serial1.available()>0){
+    incomingByte=Serial1.read();
+    Serial.write(incomingByte);
+  }
+  if(Serial.available())
+  {
+    Serial1.write(Serial.read());
+    
+  }
+  
+}
--- sketches/serial/serial.ino
+++ sketches/serial/serial.ino
@@ -7,11 +7,6 @@
 
 void loop() {
   // put your main code here, to run repeatedly:
-    //Serial.write(48);
-    //Serial. println(123,BIN);
-    //Serial. println(123,HEX);
-    //Serial. println(1.2345,3);
-    //Serial. println(F("HOLA MUNDO"));
     if(Serial.available()>0){
       incomingByte=Serial.read();
       Serial.print("Datos recibidos: ");
